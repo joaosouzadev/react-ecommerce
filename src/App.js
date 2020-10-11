6a998a75992fc2cd8d8e7f89a3a27c6552c02083
João Victor Souza <jvms3d@gmail.com>
2020-10-11 16:50:56 -0300

    Menu item components

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './App.css';
-import HomePage from './components/categories/categories';
+import HomePage from './pages/homepage/homepage';
 
 function App() {
   return (
